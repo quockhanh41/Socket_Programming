6287a18acfbe65ce3745698f248e89beeeefcc8f
quockhanh41 <115342969+quockhanh41@users.noreply.github.com>
2023-11-26 11:59:42 +0700

    add cmt

--- path_handling.py
+++ path_handling.py
@@ -13,7 +13,6 @@ def danh_sach_tap_tin(duong_dan):
     for ten in os.listdir(duong_dan):
         danh_sach_tap_tin.append(ten)
     return danh_sach_tap_tin
-    
 def path_message(path_folder):
     amount_message_cur = len(danh_sach_thu_muc(path_folder))
     newFolder = 'Message ' + str(amount_message_cur +1 ) 
@@ -21,7 +20,8 @@ def path_message(path_folder):
     if not os.path.exists(path_newFolder):
         os.makedirs(path_newFolder)
     return path_newFolder
-    
+
+# 1 tin nhắn có thể nằm trong nhiều chủ đề     
 def folder_filtering(content,project_keywords,important_keywords,work_keywords,spam_keywords):
     list_folder=[]
     From = content[content.find('From: ') + 6:content.find('To: ') - 2]
@@ -47,36 +47,16 @@ def folder_filtering(content,project_keywords,important_keywords,work_keywords,s
         list_folder.append(r"D:\MailBox\Inbox\Unread")
     return list_folder
 
-def view_all_message(path_folder):
-    path_read = os.path.join(path_folder,"Read")
-    read_list = danh_sach_thu_muc(path_read)
-    i = 1
-    for messageFolder in read_list:
-        path_messageFolder = os.path.join(path_read,messageFolder)
-        view_read_message(path_messageFolder,i)
-        i+=1
-    
-    path_unread = os.path.join(path_folder,"Unread")
-    unread_list = danh_sach_thu_muc(path_unread)
-    for messageFolder in unread_list:
-        path_messageFolder = os.path.join(path_unread,messageFolder)
-        view_unread_message(path_messageFolder,i)
-        i+=1
-
-    choice = int(input('Bạn muốn đọc Email thứ mấy: '))
-    
-    if(choice<=len(read_list)):
-        return os.path.join(path_read,read_list[choice-1])
-    else: return os.path.join(path_unread,unread_list[choice-1-len(read_list)])
-         
-
 def view_read_message(path_folder,i):
     fileList = danh_sach_tap_tin(path_folder)
     subjectFile=''
     for path in fileList:
-        if path.find('.') == -1:
+        if path.find('.') == -1: 
+            #vì tên file không dc đặt có dấu '.' 
+            #nên file không có dấu '.' là file subject
             subjectFile = path
             break
+    # read() file subject và print
     path_subjectFile = os.path.join(path_folder,subjectFile)
     with open(path_subjectFile, 'r') as subject_file:
         content = subject_file.read()
@@ -98,3 +78,26 @@ def view_unread_message(path_folder,i):
         sender = content[content.find('From: ')+6:content.find('To: ')-2]
         print(str(i)+'. (chưa đọc)',sender,subject)
     
+# hiện trạng thái tn, ng gửi và subject tất cả các tin nhắn 
+# và trả lại đường dẫn tin nhắn muốn mở 
+def view_all_message(path_folder):
+    path_read = os.path.join(path_folder,"Read")
+    read_list = danh_sach_thu_muc(path_read)
+    i = 1
+    for messageFolder in read_list:
+        path_messageFolder = os.path.join(path_read,messageFolder)
+        view_read_message(path_messageFolder,i)
+        i+=1
+    path_unread = os.path.join(path_folder,"Unread")
+    unread_list = danh_sach_thu_muc(path_unread)
+    for messageFolder in unread_list:
+        path_messageFolder = os.path.join(path_unread,messageFolder)
+        view_unread_message(path_messageFolder,i)
+        i+=1
+    # dc thì tách phần dưới ra dùm t, thấy dở quá 
+    choice = int(input('Bạn muốn đọc Email thứ mấy: '))
+    if(choice<=len(read_list)):
+        return os.path.join(path_read,read_list[choice-1])
+    else: return os.path.join(path_unread,unread_list[choice-1-len(read_list)])
+         
+
